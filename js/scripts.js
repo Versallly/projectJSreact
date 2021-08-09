55d87244b54eef714177cf8538da016a53fc1baf
Vitaly <universallly@gmail.com>
2021-08-08 23:31:31 -0400

    personaMovieDB obj and questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-console.log(1);
+const numberFoFilmes = +prompt('How many movies you have watched?');
+const movies = {};
+const personaMovieDB = {
+    count: numberFoFilmes,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Name of the one of the last movies you have watched?');
+const b = +prompt('What is your score for this movie?');
+const c = prompt('Name of the one of the last movies you have watched?');
+const d = +prompt('What is your score for this movie?');
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+console.log(movies);
+console.log(personaMovieDB);
