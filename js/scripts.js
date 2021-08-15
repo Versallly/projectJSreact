cd345c839826bdb87415cbb87d2c07e36ce7dffe
Vitaly <universallly@gmail.com>
2021-08-14 23:23:58 -0400

    functions are now methods of DB object

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,67 @@
-const numberFoFilmes = prompt('How many movies you have watched?');
+'use strict';
+let numberFoFilmes;
 const movies = {};
+
+
 const personaMovieDB = {
-    count: numberFoFilmes,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-for (let i = 1; i < 3; i++){
-    let a = prompt('Name of the one of the last movies you have watched?'),
-        b = prompt('What is your score for this movie?');
-    
-    if ( (a === null) || (b === null) || (a === '') || (b === '') || (a.length > 49 )|| (a.length > 50 ) ) {
-        i--;
-        console.log("error");
-    } else {
-        personaMovieDB.movies[a] = b;
-    }
-}
-const g = personaMovieDB.count;
+    privat: false,
+    start: function() {
+        let x = prompt('How many movies you have watched?', '');
+        while (x === '' || x === null || isNaN( x )){
+            x = prompt('How many movies you have watched?', '');
+        }
+        personaMovieDB.count = +x;
 
-if( g < 10 ){
-console.log("need more movies");
-} else if ( 10 < g && g < 30) {
-    console.log("thats good");
-} else if (g > 30){
-    console.log("enough");
-} else {
-    console.log("error");
-}
-console.log(movies);
-console.log(personaMovieDB);
+    },
+    rememberMyFilms: function (){
+        for (let i = 1; i < 3; i++){
+            let a = prompt('Name of the one of the last movies you have watched?'),
+                b = prompt('What is your score for this movie?');
+            
+            if ( (a === null) || (b === null) || (a === '') || (b === '') || (a.length > 49 )|| (a.length > 50 ) ) {
+                i--;
+                console.log("error");
+            } else {
+                personaMovieDB.movies[a] = b;
+            }
+        }
+    },
+    detectPersonalLevel: function (){
+        if( personaMovieDB.count < 10 ){
+            console.log("need more movies");
+            } else if ( 10 < personaMovieDB.count && personaMovieDB.count < 30) {
+                console.log("thats good");
+            } else if (personaMovieDB.count > 30){
+                console.log("enough");
+            } else {
+                console.log("error");
+        }
+    },
+    showMyDB: function (){
+        if (!personaMovieDB.privat) console.log( personaMovieDB );
+    },
+    writeYourGenres: function (){
+        let x,
+            i = 1;
+        while ( i-- ){
+            x = prompt(`List your favorite movies splitting them with comma`, '');
+            if ( x === null || x === '' ) i++;
+        }
+        personaMovieDB.genres = x.toLocaleLowerCase().split(', ');
+        personaMovieDB.genres.forEach( ( item, i ) => {
+            console.log(`Favorite movie # ${ i + 1 } is ${ item }`);
+        });
+    },
+    toggleVisibleMyDB: function (){
+        personaMovieDB.privat = !personaMovieDB.privat;
+    }
+
+};
+
+
+
+
